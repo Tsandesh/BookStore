afddac92631db93598ae44c507d7c6348900c047
Tsandesh <uchihazoro02@gmail.com>
2023-08-14 09:40:56 +0545

    404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/HomePage.js";
 import Explore from "./pages/Explore.js";
 import Navbar from "./components/Navbar.js";
+import Page404 from "./pages/Page404.js";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/explore" element={<Explore />} />
+        <Route path="/*" element={<Page404 />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Page404.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page404 = () => {
+  return <div>Page 404 Not Found</div>;
+};
+
+export default Page404;
