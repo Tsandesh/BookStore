74b75f09669c59048e91bd3d1c79e341a9fb592b
Tsandesh <uchihazoro02@gmail.com>
2023-08-14 09:43:12 +0545

    404 page

--- frontend/src/pages/Page404.js
+++ frontend/src/pages/Page404.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page404 = () => {
-  return <div>Page 404 Not Found</div>;
+  return <div>Page 404 Not Found Please go back to home</div>;
 };
 
 export default Page404;
